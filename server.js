5c77e101b3d723158eedc6efee07eca06e24baea
Kevin Shank <kevin_shank@yahoo.com>
2021-08-24 16:39:20 -0400

    added notes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//import all required modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -7,11 +8,12 @@ const uuid = require('./helpers/uuid');
 const app = express();
 const PORT = process.env.port || 3001;
 
-
+//add middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+//promisify read file
 const readFromFile = util.promisify(fs.readFile);
 
 const writeToFile = (destination, content) =>
@@ -19,9 +21,10 @@ const writeToFile = (destination, content) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
-
+//get html homepage
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
+//function to read and append json file
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
@@ -34,11 +37,13 @@ const readAndAppend = (content, file) => {
   });
 };
 
+//get notes html page
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
+//post for adding notes
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
@@ -50,28 +55,29 @@ app.post('/api/notes', (req, res) => {
       text,
       note_id: uuid(),
     };
-
+    //call function with note
     readAndAppend(newNote, './db/notes.json');
     res.json(`Note added successfully 🚀`);
   } else {
     res.error('Error in adding note');
   }
 });
-
+//delete for deleting notes from json file
 app.delete('/api/notes/:id', (req, res) => {
  const id = req.params.id;
-
+  //reads json file
  fs.readFile('./db/notes.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
     // Convert string into JSON object
     const parsedNotes = JSON.parse(data);
+    //filter notes and remove the note to be deleted
     const notes = parsedNotes.filter(note => note.note_id !== id);
     
     
 
-    // Write updated reviews back to the file
+    // Write updated reviews without deleted note back to the file
     fs.writeFile(
       './db/notes.json',
       JSON.stringify(notes, null, 4),
@@ -88,7 +94,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
 })});
 
-
+//get homepage for all other routes
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () =>
